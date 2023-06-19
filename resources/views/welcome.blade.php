b0ba74d9b7daf6946c98f897997a9d218e18872b
gulliano <ggulliano@msn.com>
2023-06-19 12:20:29 -0400

    installation de tailwind CSS et configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>The Shop</title>
     @livewireStyles
+    @vite('resources/css/app.css')
 </head>
 <body>
   
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
     @livewireScripts
 </body>
 </html>
