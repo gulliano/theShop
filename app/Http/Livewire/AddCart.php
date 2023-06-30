7eb5bcd76fc6f59419c04d71116a0271ed79b54a
gulliano <ggulliano@msn.com>
2023-06-30 11:00:23 -0400

    update cart

--- app/Http/Livewire/AddCart.php
+++ app/Http/Livewire/AddCart.php
@@ -11,29 +11,39 @@ class AddCart extends Component
 {
 
     public Product $product ;
+    public $quantity = 1 ;
+ 
 
     public function add()
     {
         # code...
+        if(Auth::user() == null){
 
+            return redirect(route('login')) ;
+       
+        }
       
           $cart = Cart::where('user_id', Auth::user()->id)
                         ->where('product_id', $this->product->id)
                         ->first() ;
 
         if(isset($cart)){
-
+            $cart->update([
+                'quantity' => $this->quantity ,  
+            ]) ;
         }else{
             Cart::create([
                 'user_id' => Auth::user()->id,
                 'product_id' => $this->product->id,
-                'quantity' => 1 ,
-                'prix' =>  $this->product->prix
+                'quantity' => $this->quantity ,
+                'prix' =>  $this->product->prix,
 
             ]) ;
         }
 
     }
+
+
     
     public function render()
     {
--- resources/views/livewire/add-cart.blade.php
+++ resources/views/livewire/add-cart.blade.php
@@ -1,14 +1,14 @@
 
-{{$product->name}}
+
 <div class="flex py-4 space-x-4">
     <div class="relative">
       <div class="text-center left-0 pt-2 right-0 absolute block text-xs uppercase text-gray-400 tracking-wide font-semibold">Qty</div>
-      <select class="cursor-pointer appearance-none rounded-xl border border-gray-200 pl-4 pr-8 h-14 flex items-end pb-1">
-         
-        <option>5</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
+      <select wire:model="quantity" class="cursor-pointer appearance-none rounded-xl border border-gray-200 pl-4 pr-8 h-14 flex items-end pb-1">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
       </select>
 
       <svg class="w-5 h-5 text-gray-400 absolute right-0 bottom-0 mb-2 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
